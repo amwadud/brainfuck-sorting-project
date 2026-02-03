b99d3ef5a960fef784f2a57ed29897ac31439fd2
Abdelouadoud AIT EL HAJ <amwadud@users.noreply.github.com>
2026-02-03 01:04:23 +0100

    Update the exit method.

--- src/main.c
+++ src/main.c
@@ -6,12 +6,12 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 11:45:00 by abait-el          #+#    #+#             */
-/*   Updated: 2026/01/31 02:31:47 by abait-el         ###   ########.fr       */
+/*   Updated: 2026/02/03 00:46:08 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 #if PRINT_VALS == 1
 static void ps_print_node_value(t_stack_node **node)
@@ -21,6 +21,12 @@ static void ps_print_node_value(t_stack_node **node)
 }
 #endif /* if PRINT_VALS */
 
+static void ps_error_and_exit()
+{
+	write(STDERR_FILENO, "Error\n", 6);
+	exit(-1);
+}
+
 int	main(int argc, char *argv[])
 {
 	t_stack_node *a;
@@ -29,15 +35,9 @@ int	main(int argc, char *argv[])
 	{
 		a = NULL;
 		if (!(ps_stack_init(&a, argv + 1)))
-		{
-			write(STDERR_FILENO, "Error\n", 6);
-			return (-1);
-		}
+			ps_error_and_exit();
 		if (!ps_sort(&a, true))
-		{
-			write(STDERR_FILENO, "Error\n", 6);
-			return (-1);
-		}
+			ps_error_and_exit();
 #if PRINT_VALS == 1
  		ps_stack_iter(a, ps_print_node_value);
 #endif /* if PRINT_VALS */
